3b81c8f5e919bd7d9517bb59b2b4912aba412c45
Elvis Pham <epha1206@gmail.com>
2018-04-08 21:07:30 -0400

    omdb api

--- js/liri.js
+++ js/liri.js
@@ -1,39 +1,49 @@
 require("dotenv").config({path: '../.env'});
-
-var keys = require("./keys");
-
-var Twitter = require('twitter');
-
-var spotify = new Spotify(keys.spotify);
-
-var client = new Twitter(keys.twitter);
-
 var request = require("request");
 
-console.log("keys ", keys.twitter);
+var keys = require("./keys");
+var Spotify = require('node-spotify-api');
+var Twitter = require('twitter');
+
+var twitterClient = new Twitter(keys.twitter);
+var spotifyClient = new Spotify(keys.spotify);
 
 var args = process.argv;
 
-var myTweets = args[2];
+var liriCommand = args[2];
 
-client.get('https://api.twitter.com/1.1/statuses/mentions_timeline.json', function(error, tweets, response) {
-    if(error) {
-        console.log('error', error);
-    }
-    console.log('tweets', tweets);  // The favorites. 
-    console.log(response);  // Raw response object. 
-  });
+if(liriCommand === "my-tweets") {
+    
+    twitterClient.get('https://api.twitter.com/1.1/statuses/mentions_timeline.json', function(error, tweets, response) {
+        if(error) {
+            console.log('error', error);
+        }
+        console.log('tweets', tweets);  // The favorites. 
+        console.log(response);  // Raw response object. 
+      });
+}
 
-  var Spotify = require('node-spotify-api');
- 
-  var spotify = new Spotify(keys.spotify);
-   
-  spotify
-    .request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
-    .then(function(data) {
-      console.log(data); 
-    })
-    .catch(function(err) {
-      console.error('Error occurred: ' + err); 
+  if(liriCommand === "spotify-this-song") {
+
+    var songName = process.argv[3];
+  
+    spotifyClient.search({ type: 'track', query: songName, limit: 1}, function(err, data) {
+      if (err) {
+        return console.log('Error occurred: ' + err);
+      }
+     
+    console.log("song", data.tracks.items[0]); 
     });
+  }
 
+if(liriCommand === "movie-this") {
+    var movieName = process.argv[3];
+
+    var queryUrl = "http://www.omdbapi.com/?apikey=c9c28c3b&t=" + movieName;
+    
+    request(queryUrl, function (error, response, body) {
+      console.log('error:', error); // Print the error if one occurred
+      console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+      console.log('body:', body); // Print the HTML for the Google homepage.
+    });
+}
